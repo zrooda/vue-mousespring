cfa46c4a10e6ec65700d3f052aca3e57ce4e88de
mystrdat <mystrdat@gmail.com>
2020-09-19 12:50:54 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # vue-mousespring
+
+A Vue component that uses `vue-motion` spring physics and mouse coordinates on the layer to either:
+- Tilt an element passed as a slot child
+- Pass spring-smoothed mouse X/Y coordinates (range in 0 - 1) that you can access with [scoped-slots](https://vuejs.org/v2/guide/components-slots.html#Scoped-Slots)
+
+Includes a `mapRange` helper function to simplify adding motion based on the mouse coords.
+
+Root element size changes are automatically updated using a [ResizeObserver](https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserver).
+
+## Installation
+
+```Node
+npm i --save mystrdat/vue-mousespring
+```
+
+```Vue
+<template>
+  <Motion v-slot="{ coords, mapRange }">
+    <div class="container">
+      <div
+        class="something-else"
+        :style="{
+          transform: `translateX(${mapRange(5, -5, coords.y)}px) translateY(${mapRange(-5, 5, coords.x)}px)`
+        }"
+      />
+    </div>
+  </Motion>
+</template>
+
+<script>
+import MouseSpring from 'vue-mousespring'
+
+export default {
+  components: {
+    MouseSpring
+  }
+}
+</script>
+```
+
+## Options
+
+```Vue
+<MouseSpring
+  :tilt="true"
+  :default-coords="{ x: 0.5, y: 0.5 }"
+  :x-range="[5, -5]"
+  :y-range="[-5, 5]"
+  :spring="{ stiffness: 60, damping: 16, precision: 0.01 }"
+  :leave-reset="200"
+>
+```
+
+`tilt` - Use default tilt mechanics | `Boolean` | default `true`  
+`defaultCoords` - Default mouse coordinates | `Object` | default `{ x: 0.5, y: 0.5 }`  
+`xRange` - Default tilt angle range on X-axis in degrees | `Array` | default `[5, -5]`  
+`yRange` - Default tilt angle range on Y-axis in degrees | `Array` | default `[-5, 5]`  
+`spring` - Spring configuration, see [options](https://posva.net/vue-motion/#/home?id=springs) | `String, Object` | default `{ stiffness: 60, damping: 16, precision: 0.01 }`  
+`leaveReset` - Timer after which to reset to default coords when the pointer leaves element | `Number` | default `200`
---
+++ src/MouseSpring.vue
@@ -0,0 +1,128 @@
+<template>
+  <Motion
+    v-slot="_coords"
+    ref="container"
+    tag="div"
+    class="mouse-spring"
+    :spring="spring"
+    :values="coords"
+    @mousemove.native="mouseMove"
+    @mouseleave.native="mouseLeave"
+  >
+    <div
+      class="layer"
+      :style="{
+        transform: tilt ? `rotateX(${getXRotation(_coords.y)}deg) rotateY(${getYRotation(_coords.x)}deg)` : 'none'
+      }"
+    >
+      <slot
+        :coords="_coords"
+        :mapRange="mapRange"
+      />
+    </div>
+  </Motion>
+</template>
+
+<script>
+import { Motion } from 'vue-motion'
+import clamp from 'lodash.clamp'
+import throttle from 'lodash.throttle'
+
+export default {
+  components: {
+    Motion
+  },
+  props: {
+    tilt: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    defaultCoords: {
+      type: Object,
+      required: false,
+      default: () => ({ x: 0.5, y: 0.5 })
+    },
+    xRange: {
+      type: Array,
+      required: false,
+      default: () => ([5, -5])
+    },
+    yRange: {
+      type: Array,
+      required: false,
+      default: () => ([-5, 5])
+    },
+    spring: {
+      type: [String, Object],
+      required: false,
+      default: () => ({ stiffness: 60, damping: 16, precision: 0.01 })
+    },
+    leaveReset: {
+      type: Number,
+      required: false,
+      default: 200
+    }
+  },
+  data () {
+    return {
+      container: {
+        width: 0,
+        height: 0
+      },
+      coords: this.defaultCoords
+    }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      this.resizeObserver = new ResizeObserver((entries) => {
+        entries.forEach((mutation) => {
+          this.container = {
+            width: mutation.contentRect.width,
+            height: mutation.contentRect.height
+          }
+        })
+      })
+      this.resizeObserver.observe(this.$refs.container.$el)
+    })
+  },
+  destroyed () {
+    this.resizeObserver.disconnect()
+  },
+  methods: {
+    mouseMove: throttle(function (e) {
+      this.coords = {
+        x: clamp(e.offsetX / this.container.width, 0, 1),
+        y: clamp(e.offsetY / this.container.height, 0, 1)
+      }
+    }, 10),
+    mouseLeave () {
+      setTimeout(() => {
+        this.coords = this.defaultCoords
+      }, this.leaveReset)
+    },
+    getXRotation (coord) {
+      return this.mapRange(this.xRange[0], this.xRange[1], coord)
+    },
+    getYRotation (coord) {
+      return this.mapRange(this.yRange[0], this.yRange[1], coord)
+    },
+    mapRange (min, max, val) {
+      return (val - 0) * (y2 - x2) / (1 - 0) + x2
+    }
+  }
+}
+</script>
+
+<style scoped>
+.mouse-spring {
+  position: relative;
+  perspective: 1000px;
+}
+
+.layer {
+  width: inherit;
+  height: inherit;
+  will-change: transform;
+}
+</style>
